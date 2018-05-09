ea05249382c57fed596884dc558544b23d6740a1
Riccardo Serafini <tapion@tapion.it>
2018-05-09 18:36:52 +0200

    qualche view

--- web/views.py
+++ web/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def index(request):
+    return render(request, 'web/index.html', {})
+
+def login(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/')
+    return render(request, 'web/login.html', {})
